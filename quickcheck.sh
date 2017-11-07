e26d4ae6e2700a75c336b7fba6d6b03dde47cb1b
Yann Pouillon <devops@materialsevolution.es>
2017-11-07 12:52:42 +0100

    Fixed quick checker script loading an inconsistent build environment

--- quickcheck.sh
+++ quickcheck.sh
@@ -3,6 +3,7 @@
 . /usr/share/lmod/lmod/init/bash
 module use /home/pouillon/retos/HPC/modules/all
 module load foss/2016b
+module load HDF5/1.8.17-foss-2016b
 
 set -e
 
@@ -14,7 +15,9 @@ mkdir tmp
 cd tmp
 ../configure \
   --enable-local-build \
-  --with-linalg-libs="-lopenblas"
+  --with-linalg-libs="-lopenblas" \
+  CC="gcc" \
+  FC="gfortran"
 
 test "${cmd}" != "no-make" && make dist
 test "${cmd}" != "no-make" && make -j4
