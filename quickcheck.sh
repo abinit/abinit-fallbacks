588cf3eeba8f64f1b9781449017c185d944400c3
Yann Pouillon <devops@materialsevolution.es>
2018-02-20 13:54:39 +0100

    Added basic CI capabilities

--- quickcheck.sh
+++ quickcheck.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
+# Init build environment
 . /usr/share/lmod/lmod/init/bash
-module use /home/pouillon/retos/HPC/modules/all
+module use /home/pouillon/retos/hpc/modules/all
+module purge
 module load foss/2016b
 module load HDF5/1.8.17-foss-2016b
 
+# Stop at first error
 set -e
 
+# Init source tree
 cmd="${1}"
-
 ./wipeout.sh
 ./autogen.sh
+
+# Build using external linear algebra
 mkdir tmp
 cd tmp
 ../configure \
   --enable-local-build \
   --with-linalg-libs="-lopenblas" \
+  CC="gcc" \
+  FC="gfortran"
+
+test "${cmd}" != "no-make" && make dist
+test "${cmd}" != "no-make" && make -j4
+test "${cmd}" != "no-make" && make install
+
+# Build only internal linear algebra
+cd ..
+mkdir tmp-linalg
+cd tmp-linalg
+../configure \
+  --enable-local-build \
+  --enable-linalg \
+  --disable-atompaw \
+  --disable-bigdft \
+  --disable-libpsml \
+  --disable-libxc \
+  --disable-netcdf4 \
+  --disable-netcdf4-fortran \
+  --disable-wannier90 \
+  --disable-xmlf90 \
+  --disable-yaml \
   CC="gcc" \
   FC="gfortran"
 
